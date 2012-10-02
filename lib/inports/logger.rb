842376bae1696394432340261eb9fc91ba3626c7
Rob McGrail <rob@cwa-rob-macbook.(none)>
2012-10-02 15:53:22 +1300

    Fixing line breaks in logger.

--- lib/inports/logger.rb
+++ lib/inports/logger.rb
@@ -3,12 +3,17 @@ module Logger
   # Logs and prints a warning message.
   # Requires a type (which is the name of the log file the
   # warning will be stored as.
+  #
+  # Logger.warning(path, 'no-heading')
+  #
+  # $ tail ./log/no-heading.log
+  # $ ./curriculum/index.htm
 
   def self.warning(path, type)
     log_file = './log/' + type + '.log'
 
     puts $term.color("Logging item as #{type} => #{log_file}", :yellow)
 
-    File.open(log_file, 'a') {|f| f.write(path + '\n') }
+    File.open(log_file, 'a') {|f| f.write(path + "\n") }
   end
 end
