9033a2bc5d71de8257e6e38599401c56b2341720
Rob McGrail <rob@cwa-rob-macbook.(none)>
2012-10-10 09:29:41 +1300

    basic tests for handle loop.

--- test/test_ez_object.rb
+++ test/test_ez_object.rb
@@ -2,22 +2,54 @@ require './lib/inports'
 Bundler.require(:test)
 
 class TestEzObject < MiniTest::Unit::TestCase
+  def setup
+    eval %{
+
+      class EzShort < EzObject
+        def self.mine?(path)
+          true if path.length < 2
+        end
+
+        def self.store(path)
+          $r.set path, self
+          $r.log_key path
+        end
+      end
+
+
+      class EzHello < EzObject
+        def self.mine?(path)
+          true if path == 'hello'
+        end
+
+        def self.store(path)
+          $r.set path, self
+          $r.log_key path
+        end
+      end
+
+    }
+  end
+
+
   def teardown
     $r.kill_keys
   end
 
 
   def test_inheriting_from_class_registers_class_as_descendent
-    eval "class Thing < EzObject; end"
-    assert_includes EzObject.inherited('dummy class'), Thing
+    assert_includes EzObject.inherited('dummy class'), EzShort
   end
 
 
-  def test_handle_iterates_through_descendants
+  def test_handle_iterates_through_descendants_calling_mine
+    EzObject.handle('hello')
+    assert_equal "TestEzObject::EzHello", $r.get('hello')
 
-    # Going to need to mock the file structure so there are paths.
-
+    EzObject.handle('x')
+    assert_equal "TestEzObject::EzShort", $r.get('x')
   end
+
 
 
   def test_parent_id_returns_parents_id
@@ -28,7 +60,9 @@ class TestEzObject < MiniTest::Unit::TestCase
     load './lib/inports/redis.rb'
 
     $r.hset './test/hello', 'id', $r.get_id
+    $r.log_key './test/hello'
     $r.hset './test/another', 'id', $r.get_id
+    $r.log_key './test/another'
 
     assert_equal '51', EzObject.parent_id('./test/hello/index.htm')
     assert_equal '51', EzObject.parent_id('./test/hello/thing.htm')
