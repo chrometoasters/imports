a3a8fe717fd1c604a1b44d7d3c6aacb8bf571403
Rob McGrail <rob@cwa-rob-macbook.(none)>
2012-10-10 09:01:18 +1300

    EzObject tests.

--- test/test_ez_object.rb
+++ test/test_ez_object.rb
@@ -2,12 +2,35 @@ require './lib/inports'
 Bundler.require(:test)
 
 class TestEzObject < MiniTest::Unit::TestCase
-  def test_inheriting_from_class_registers_class_as_descendent
-    # class Thing < EzObject; end
-
-    # Use eval dummy.
-    assert_includes Thing, EzObject.inherited('dummy')
+  def teardown
+    $r.kill_keys
   end
 
 
+  def test_inheriting_from_class_registers_class_as_descendent
+    eval "class Thing < EzObject; end"
+    assert_includes EzObject.inherited('dummy class'), Thing
+  end
+
+
+  def test_handle_iterates_through_descendants
+
+    # Going to need to mock the file structure so there are paths.
+
+  end
+
+
+  def test_parent_id_returns_homepage_id_from_config_for_root
+    CONFIG['directories']['input'] = './thing'
+    CONFIG['ids']['homepage'] = 1
+
+    load './lib/inports/redis.rb'
+
+    assert_equal '1', EzObject.parent_id('./thing/what')
+  end
+
+
+  def test_parent_id_fails_on_orphaned_path
+    assert_raises(Orphanity) { EzObject.parent_id('z') }
+  end
 end
