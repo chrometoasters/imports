2e1673c439c62897eb16ff3096ea7c521b84231d
Rob McGrail <rob@cwa-rob-macbook.(none)>
2012-10-11 17:11:49 +1300

    Adding subheadings and removing redundant strongs from headings.

--- lib/inports/sanitize_transformers/headings.rb
+++ lib/inports/sanitize_transformers/headings.rb
@@ -1,5 +1,5 @@
 # Convert <p class="subhead"> => <heading level="2">
-SubheadToHeading = lambda do |env|
+SubheadToHeading2 = lambda do |env|
   node = env[:node]
   name = env[:node_name]
   return if env[:is_whitelisted] || !node.element?
@@ -10,5 +10,31 @@ SubheadToHeading = lambda do |env|
   end
 end
 
-Headings = [SubheadToHeading]
+
+# Convert <p class="subsubhead"> => <heading level="2">
+SubsubheadToHeading3 = lambda do |env|
+  node = env[:node]
+  name = env[:node_name]
+  return if env[:is_whitelisted] || !node.element?
+
+  if node[:class] == 'subsubhead' && name == 'p'
+    node.name = 'heading'
+    node[:level] = '3'
+  end
+end
+
+
+# Convert <heading><strong> => <heading>
+RemoveRedundantStrongs = lambda do |env|
+  node = env[:node]
+  name = env[:node_name]
+  return if env[:is_whitelisted] || !node.element?
+
+  if name == 'heading' && node.child.name == 'strong'
+    node.child.swap(node.child.children)
+  end
+end
+
+
+Headings = [SubheadToHeading2, SubsubheadToHeading3, RemoveRedundantStrongs]
 
