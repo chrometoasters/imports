e9a198fe728e984b8013f973d3b66579b799e8eb
Rob McGrail <rob@cwa-rob-macbook.(none)>
2012-10-11 13:14:22 +1300

    Basic sanitize config and loading.

--- lib/inports.rb
+++ lib/inports.rb
@@ -3,24 +3,29 @@ require 'bundler/setup'
 
 Bundler.require(:default)
 
+
 # Create config object
 raw_config = File.read('./config.yml')
 CONFIG = YAML.load(raw_config)
 
+
 # Create terminal
 $term = HighLine.new
 
-# load helpers
 
+# load helpers
 Dir['./lib/helpers/*.rb'].each {|file| require file }
 
-# load sanitize configurations
 
-Dir['./lib/sanitize_configs/*.rb'].each {|file| require file }
+# load sanitize transformers
+Dir['./lib/inports/sanitize_transformers/*.rb'].each {|file| require file }
+
+
+# load sanitize configurations
+Dir['./lib/inports/sanitize_configs/*.rb'].each {|file| require file }
 
 
 # load code
-
 require './lib/inports/string'
 require './lib/inports/exceptions'
 require './lib/inports/redis'
