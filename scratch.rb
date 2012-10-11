dea5d74603614f193ec73f56e41bfa89dfbef10f
Rob McGrail <rob@cwa-rob-macbook.(none)>
2012-10-11 17:12:32 +1300

    scratchpad changes

--- scratch.rb
+++ scratch.rb
@@ -1,10 +1,12 @@
 require './lib/inports'
 include Convert
 
-file = File.open('./input/curriculum-support/terms.htm')
+file = File.open('./input/curriculum-support/Strategies/tp-brief/index.htm')
 contents = file.read
 
 str = to_ezp contents
 
 
-puts str
+puts str if $verbose
+
+# puts Crawler.new.list
