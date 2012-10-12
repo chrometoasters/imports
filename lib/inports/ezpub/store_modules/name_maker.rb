af80b4fb461b5305ffc9a2f13bf1b26c568708fd
Rob McGrail <rob@cwa-rob-macbook.(none)>
2012-10-12 16:26:53 +1300

    Extension for deriving object names.

---
+++ lib/inports/ezpub/store_modules/name_maker.rb
@@ -0,0 +1,16 @@
+module NameMaker
+  def pretify_filename(path)
+    # Get just the filename.
+    path = /([^\/]+\.\w+)$/.match(path)[0]
+
+    # Remove extension
+    path.gsub!(/\.\w+$/, '')
+
+    # Replace characters with spaces.
+    path.gsub!(/-|_|\./, ' ')
+
+    # Sentence case
+    path.downcase!
+    path.slice(0,1).capitalize + path.slice(1..-1)
+  end
+end
