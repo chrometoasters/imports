d3d504e649c130d34cf45264689483cbba76a8ad
Rob McGrail <rob@cwa-rob-macbook.(none)>
2012-10-12 13:58:32 +1300

    Adding locations for various helpers for class identification and storage.

--- lib/inports/ezpub.rb
+++ lib/inports/ezpub.rb
@@ -1,5 +1,11 @@
 require './lib/inports/ezpub/handler'
 require './lib/inports/ezpub/handlers'
 
+# load helpers for mine? methods.
+Dir['./lib/inports/ezpub/mine_modules/*.rb'].each {|file| require file }
+
+# load helpers for store methods.
+Dir['./lib/inports/ezpub/store_modules/*.rb'].each {|file| require file }
+
 require './lib/inports/ezpub/image'
 require './lib/inports/ezpub/file'
