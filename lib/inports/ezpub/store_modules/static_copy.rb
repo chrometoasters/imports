d7db65aed49479b637b1616c4088387b61c2904a
Rob McGrail <rob@cwa-rob-macbook.(none)>
2012-10-12 16:28:08 +1300

    Helper for copying static files, mixin for handlers.

--- lib/inports/ezpub/store_modules/static_copy.rb
+++ lib/inports/ezpub/store_modules/static_copy.rb
@@ -1,3 +1,42 @@
 module StaticCopy
 
+  # Helper mixin for copying static files.
+
+  def move_to_files(path)
+    move_to(path, CONFIG['directories']['output']['files'])
+  end
+
+
+  def move_to_images(path)
+    move_to(path, CONFIG['directories']['output']['images'])
+  end
+
+
+  def move_to(path, dest)
+    name = flatten(path)
+    dest = dest + '/' + name
+
+    if File.exists? dest
+      Logger.warning path, 'Duplicate filename'
+      dest = make_unique(dest)
+    end
+
+    FileUtils.cp(path, dest)
+    dest
+  end
+
+
+  # Goes from ./thing/folder/hello.jpg => hello.jpg
+
+  def flatten(path)
+    /([^\/]+\.\w+)$/.match(path)[0]
+  end
+
+
+  # When a filename needs uniquifying,
+  # Adds current timestamp at end of filename.
+
+  def make_unique(path)
+    path.gsub(/(\.\w+$)/, "#{Time.now.to_i}" + '\1')
+  end
 end
