6c9e3204e803e43b7b045e20eef3fc4301dd6028
Rob McGrail <rob@cwa-rob-macbook.(none)>
2012-10-15 17:44:50 +1300

    Removing nonsense.

--- scratch.rb
+++ scratch.rb
@@ -1,10 +1,6 @@
 require './lib/inports'
 include Convert
 
-file = File.open('./input/curriculum-support/Strategies/tp-brief/index.htm')
-contents = file.read
-
-str = to_ezp contents
 
 
 puts str if $verbose
