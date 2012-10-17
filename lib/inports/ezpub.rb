bb12a6ac734430e8d3805bed079c33d6353379a0
Rob McGrail <rob@cwa-rob-macbook.(none)>
2012-10-18 12:18:10 +1300

    Manually ordering helpers.

--- lib/inports/ezpub.rb
+++ lib/inports/ezpub.rb
@@ -1,15 +1,21 @@
 require './lib/inports/ezpub/handler'
 require './lib/inports/ezpub/handler_sets'
 
-# load helpers for mine? methods.
-Dir['./lib/inports/ezpub/mine_modules/*.rb'].each {|file| require file }
 
-# load helpers for store methods.
+# load helpers for mine and store methods.
+require './lib/inports/ezpub/mine_modules/ezp_extensions.rb'
+require './lib/inports/ezpub/mine_modules/is_a_redirect.rb'
+require './lib/inports/ezpub/mine_modules/has_valid_index.rb'
+require './lib/inports/ezpub/mine_modules/include_or_page.rb'
+
 Dir['./lib/inports/ezpub/store_modules/*.rb'].each {|file| require file }
 
 require './lib/inports/ezpub/media_folder'
+require './lib/inports/ezpub/thumbnail'
 require './lib/inports/ezpub/image'
 require './lib/inports/ezpub/file'
+require './lib/inports/ezpub/redirect'
+
 
 require './lib/inports/ezpub/folder'
 require './lib/inports/ezpub/general_content'
