be459d60afd41acd0f12c98711006bf32ddf7ae6
Rob McGrail <rob@cwa-rob-macbook.(none)>
2012-10-18 12:17:45 +1300

    Renaming xml file.

--- bin/static.rb
+++ bin/static.rb
@@ -12,6 +12,6 @@ puts $term.color("Static file ingest complete.", :green)
 
 puts $term.color("Generating XML...", :green)
 
-path = @p.to_xml :name => 'all'
+path = @p.to_xml :name => 'techlink-images-and-files'
 
 puts $term.color("XML created at #{path}.", :green)
