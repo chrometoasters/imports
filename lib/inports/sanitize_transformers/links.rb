6ad1654becf028b12efba7a7d7c48e527640e762
Rob McGrail <rob@cwa-rob-macbook.(none)>
2012-10-18 17:41:17 +1300

    Adding internal link handling - unfinished.

--- lib/inports/sanitize_transformers/links.rb
+++ lib/inports/sanitize_transformers/links.rb
@@ -46,4 +46,37 @@ ExternalLink = lambda do |env|
 end
 
 
-Links = [AnchorEndPoint, AnchorLink, Mail, ExternalLink]
+# <a href="/somehwere/internal.htm"></a> => <link href="eznode://0f2d5c080f60022be272ff2fd911cbca"></link>
+InternalAbsoluteLink = lambda do |env|
+  node = env[:node]
+  name = env[:node_name]
+  return if env[:is_whitelisted] || !node.element?
+
+  if name == 'a' && node[:href] =~  /^\//
+    node.name = 'link'
+
+    path = node[:href]
+
+    # unless path is in array of forbidden paths....
+
+    path.gsub!(/#\w+$/, '')
+
+    key = CONFIG['directories']['input'] + path
+
+    new_href = $r.hget(key, 'id')
+
+    if new_href
+      new_href = 'eznode://' + new_href
+
+      puts new_href
+      puts node[:href]
+
+      node[:href] = new_href
+    else
+      raise UnresolvedInternalLink, "Couldn't resolve #{node[:href]}"
+    end
+  end
+end
+
+
+Links = [AnchorEndPoint, AnchorLink, Mail, InternalAbsoluteLink, ExternalLink]
