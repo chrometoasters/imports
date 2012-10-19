9ce902514558d1d5979cbece9039a2b8067ccc9e
Rob McGrail <rob@cwa-rob-macbook.(none)>
2012-10-19 15:45:19 +1300

    Provide catchall extensions list.

--- lib/inports/ezpub/mine_modules/ezp_extensions.rb
+++ lib/inports/ezpub/mine_modules/ezp_extensions.rb
@@ -1,2 +1,3 @@
 EZP_ICON_BINARY_EXTENSIONS = %w[doc docx mp3 pdf ppt pps ppsx pptx rtf wav xls xlsx zip pub rar]
 EZP_ICON_IMAGE_EXTENSIONS = %w[jpg jpeg gif bmp png]
+EZP_FILE_EXTENSIONS = EZP_ICON_BINARY_EXTENSIONS + EZP_ICON_IMAGE_EXTENSIONS
