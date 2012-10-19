5eea94401e54c68efaeed43b8ac10289e31997fe
Rob McGrail <rob@cwa-rob-macbook.(none)>
2012-10-19 15:46:06 +1300

    Troubleshooting.

--- scratch.rb
+++ scratch.rb
@@ -2,11 +2,11 @@ require './lib/inports'
 
 $r.kill_keys
 
-# # puts EzPub::File.mine?('./input/curriculum-support/pdfs/tl-learning-progression-diagrams-oct-2010.pdf')
+puts EzPub::File.mine?('./input/latest-news/resources/articles/teched-key.pdf')
 
 # include IsARedirect
 
 # redirect?('./input/curriculum-support/CSP/index.htm')
 
 
-DatabaseImporters::Glossary.run
+#DatabaseImporters::Glossary.run
