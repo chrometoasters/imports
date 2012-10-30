c7cd8c9c0351756345fe759cc384a71d5039011f
Rob McGrail <rob@cwa-rob-macbook.(none)>
2012-10-30 16:36:28 +1300

    Scratch updates.

--- scratch.rb
+++ scratch.rb
@@ -1,6 +1,6 @@
 require './lib/inports'
 
-$r.kill_keys
+# $r.kill_keys
 
 # #puts EzPub::File.mine?('./input/curriculum-support/Teacher-Education/PTTER-framework/E1/E1A2.htm')
 
@@ -24,7 +24,6 @@ $r.kill_keys
 # post_process
 
 
-# include PostProcessor
 
 
 # str = StringFromPath.get_case_insensitive('./input/Case-studies/Technological-practice/Soft-Materials/Conscious-Cloth/index.htm')
@@ -32,18 +31,41 @@ $r.kill_keys
 
 # puts to_ezp(str, :config => Sanitize::InportConfig::EZXML)
 
-path = './input/Case-studies/Technological-practice/Food-and-Biological/Gluten-free-cookies/'
+path = './input/curriculum-support/indicators/practice/'
 
-puts EzPub::CaseStudy.mine?(path)
-EzPub::CaseStudy.store(path)
+
+puts EzPub::GeneralContent.mine?(path)
+EzPub::GeneralContent.store(path)
 
 include PostProcessor
 post_process
 
-# puts $r.hget(path, 'field_body')
 puts $r.hget(path, 'field_title')
-puts $r.hget(path, 'field_category')
-puts $r.hget(path, 'field_case_study_date')
-puts $r.hget(path, 'field_year_level')
-puts $r.hget(path, 'field_cover_image')
+puts $r.hget(path, 'field_body')
 
+# puts $r.hget(path, 'field_body')
+
+# Processor.new.list.each do |path|
+#   puts path
+#   str = StringFromPath.get_case_insensitive(path)
+#   path.gsub!('./input', 'http://www.techlink.org.nz')
+#   Sanitize.clean(str, :transformers => [ClassAttributeInspector], :path => path)
+# end
+
+# puts $r.hget(path, 'field_category')
+# puts $r.hget(path, 'field_case_study_date')
+# puts $r.hget(path, 'field_year_level')
+# puts $r.hget(path, 'field_cover_image')
+
+
+# File.open('classes.txt', 'a') do |f|
+#   $r.smembers('classes').each do |line|
+#     f.write(line + "\n")
+#   end
+# end
+
+# File.open('examples.txt', 'a') do |f|
+#   $r.smembers('examples').each do |line|
+#     f.write(line + "\n")
+#   end
+# end
