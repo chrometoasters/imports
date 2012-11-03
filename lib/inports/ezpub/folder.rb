b7b2ca0b05d408c88549f4b4236f315dc161afd1
Rob McGrail <rob@cwa-rob-macbook.(none)>
2012-11-03 17:13:25 +1300

    Improving folder rekeying logic.

--- lib/inports/ezpub/folder.rb
+++ lib/inports/ezpub/folder.rb
@@ -32,47 +32,80 @@ module EzPub
 
       path = path + '/' unless path =~ /\/$/
 
-      # Give precedence to key that already exists.
-      #
-      # For more information see ActsAsIndex module.
+      # # Give precedence to key that already exists.
+      # #
+      # # For more information see ActsAsIndex module.
 
-      if $r.exists(path)
-        return nil
-      end
+      # if $r.exists(path)
+      #   return nil
+      # end
 
 
       # Rekey another item if this folder's index redirects to that item.
       if has_remote_index? path
+
+        puts 'has_remote index!'
+
+        puts  path + '     path'
+
         replacement = has_remote_index? path
 
+        puts replacement + '     replacement'
+
         # Check if its referent exists, if so, simply key as that item.
-        # So that anything calling #parent_id gets this remote id.
+        # So that anything calling #parent_id gets its remote id.
         if $r.hget(replacement, 'id')
-          new_id = $r.hget(replacement, 'id')
 
-          $r.hset path, 'id', new_id
-        end
-
-        if !$r.hget(replacement, 'id') && page?(replacement)
-          # In the event that we're keying a page that doesn't exist yet,
-          # we check it's a valid page, and that it's a valid page.
-          # We then set its id ahead of time, which $redis.get_id(path) will
-          # now respect, and return.
-
-          $r.hset replacement, 'id', new_id
-
-          # Now set this key as having that id also, so that #get_parent(path)
-          # will still function as expected.
+          puts '     replacement id response:'
+          puts $r.hget(replacement, 'id')
 
           new_id = $r.hget(replacement, 'id')
 
           $r.hset path, 'id', new_id
 
           # And remove this folder from the keys list so it isn't in the xml.
-          $r.lrem 'keys', 1, path
+          # $r.lrem 'keys', 1, path
+          $r.rpush 'pseudo-keys', path
+        else
+          puts 'no replacement!'
 
-          puts path
-          # Check that that actuall works.
+          unless page?(replacement)
+            raise "Attempting a rekey but endpoint is not a page (#{replacement})"
+          end
+
+
+          # In the event that we're keying a page that doesn't exist yet,
+          # we check it's a valid page.
+
+          # We then set its id ahead of time, which $redis.get_id(path) will
+          # now respect, and return.
+
+          replacement_id = $r.get_id(replacement)
+
+          puts replacement_id + "     replacement's get_id"
+
+          $r.hset replacement, 'id', replacement_id
+
+          # Same for parent
+
+          $r.hset replacement, 'parent', parent_id(path)
+
+          # Now set this key as having that id also, so that #get_parent(path)
+          # will still function as expected.
+
+          confirmed_replacement_id = $r.hget(replacement, 'id')
+
+          if confirmed_replacement_id != replacement_id
+            raise "Trying to use a pseudo id replacement but we're out of sync at #{path} and #{replacement}"
+          end
+
+          # Set the id for this key so it's there for parent_id calls.
+          $r.hset path, 'id', replacement_id
+
+          # And add this folder to the keys list - this becomes its position in the heirarchy.
+          # (Later handlers wont add it to the list again, and if they do it will register as an error
+          # during our sanity checks)
+          $r.log_key(path)
         end
 
         Logger.warning path, 'Underwent remote index rekey'
